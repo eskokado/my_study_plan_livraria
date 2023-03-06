f47d1b8875eb72c1f3ec4c904b02afcfdfc2fbf9
eskokado <eskokado@gmail.com>
2023-03-06 08:40:39 -0300

    feat: create rspec of the model Account

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Account, type: :model do
+  it 'is valid all attributes' do
+    account = create(:account)
+    expect(account).to be_valid
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:account_number) }
+    it { is_expected.to belong_to :supplier }
+  end
+end
