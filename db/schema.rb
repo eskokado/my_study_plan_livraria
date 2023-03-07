6d83b0914fa665afc140e97b3a1aea01f59ac089
eskokado <eskokado@gmail.com>
2023-03-07 05:17:23 -0300

    feat: add model Book into schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_064413) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_072739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_064413) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.datetime "published_at"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -35,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_064413) do
   end
 
   add_foreign_key "accounts", "suppliers"
+  add_foreign_key "books", "authors"
 end
