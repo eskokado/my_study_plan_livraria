1631902f4698790db12ff587f59d44bfdd83c722
eskokado <eskokado@gmail.com>
2023-03-07 05:18:04 -0300

    test: create rspec of model Book

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it 'is valid all attributes' do
+    book = create(:book)
+    expect(book).to be_valid
+  end
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:published_at) }
+    it { is_expected.to belong_to :author }
+  end
+end
