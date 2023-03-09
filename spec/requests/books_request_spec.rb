954853ee4b324bcc31d61ca3df5272de752c20dd
eskokado <eskokado@gmail.com>
2023-03-09 05:27:24 -0300

    test: Book with valid params return last added Book

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -11,5 +11,12 @@ RSpec.describe "Books", type: :request do
         post url, params: book_params
       end.to change(Book, :count).by(1)
     end
+
+    it 'returns last added Book' do
+      post url, params: book_params
+      expected_book = Book.last.as_json(only: %i(id name))
+      expect(body_json['book']).to eq expected_book
+    end
+
   end
 end
