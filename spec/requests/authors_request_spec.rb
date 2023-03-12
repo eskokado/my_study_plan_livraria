90ba41dc167b0cf70116d6aec102c3ddcc03bf14
eskokado <eskokado@gmail.com>
2023-03-12 09:13:28 -0300

    test: POST author #create with valid parameters create a new author

--- spec/requests/authors_request_spec.rb
+++ spec/requests/authors_request_spec.rb
@@ -1,48 +1,14 @@
 require 'rails_helper'
+RSpec.describe AuthorsController, type: :controller do
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      let(:valid_params) { { author: { name: 'J.K. Rowling' } } }
 
-RSpec.describe "Authors", type: :request do
-  let(:url) { "/authors" }
-
-  context "with valid params" do
-    let(:author_params) { { author: attributes_for(:author) } }
-
-    it 'adds a new Author' do
-      expect do
-        post url, params: author_params
-      end.to change(Author, :count).by(1)
-    end
-
-    it 'returns last added Author' do
-      post url, params: author_params
-      expected_author = Author.last.as_json(only: %i(id name))
-      expect(body_json['author']).to eq expected_author
-    end
-
-    it 'returns success status' do
-      post url, params: author_params
-      expect(response).to have_http_status(:created)
-    end
-  end
-
-  context "with invalid params" do
-    let(:author_invalid_params) do
-      { author: attributes_for(:author, name: nil) }.to_json
-    end
-
-    it 'does not add a new Author' do
-      expect do
-        post url, params: author_invalid_params
-      end.to_not change(Author, :count)
-    end
-
-    it 'returns error message' do
-      post url, params: author_invalid_params
-      expect(body_json['errors']['fields']).to have_key('name')
-    end
-
-    it 'returns unprocessable_entity status' do
-      post url, params: author_invalid_params
-      expect(response).to have_http_status(:unprocessable_entity)
+      it 'creates a new author' do
+        expect {
+          post :create, params: valid_params
+        }.to change(Author, :count).by(1)
+      end
     end
   end
 end
