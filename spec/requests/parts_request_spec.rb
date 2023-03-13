6427b3804a0e7abebca6f34e4304800f504f5cb7
eskokado <eskokado@gmail.com>
2023-03-13 08:06:21 -0300

    test: POST #create with valid params returns a success response with JSON body

--- spec/requests/parts_request_spec.rb
+++ spec/requests/parts_request_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe PartsController, type: :controller do
   let!(:supplier) { create(:supplier) }
 
@@ -9,6 +11,14 @@ RSpec.describe PartsController, type: :controller do
         expect {
           post :create, params: { supplier_id: supplier.id, part: part_attributes }
         }.to change(Part, :count).by(1)
+      end
+
+      it "returns a success response with JSON body" do
+        post :create, params: { supplier_id: supplier, part: part_attributes }
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+        expect(JSON.parse(response.body)['part']['part_number']).to eq(part_attributes[:part_number])
+        expect(JSON.parse(response.body)['part']['supplier_id']).to eq(supplier.id)
       end
     end
   end
