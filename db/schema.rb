fe5375c48a45c4af105fc1cbc6e303e7a005cde8
eskokado <eskokado@gmail.com>
2023-03-13 07:39:07 -0300

    feat: create migration of the relation parts and suppliers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_092119) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_094913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_092119) do
     t.string "part_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "supplier_id", null: false
+    t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
@@ -67,4 +69,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_092119) do
   add_foreign_key "assemblies_parts", "assemblies"
   add_foreign_key "assemblies_parts", "parts"
   add_foreign_key "books", "authors"
+  add_foreign_key "parts", "suppliers"
 end
