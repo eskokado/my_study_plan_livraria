69f98f87c38d9783d90014c8a9a657eab590bab7
eskokado <eskokado@gmail.com>
2023-03-15 19:37:15 -0300

    feat: add model AssembliesBooks and relacions assembly and book

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -2,4 +2,5 @@ class Assembly < ApplicationRecord
   validates :name, presence: true
 
   has_many :assemblies_parts
+  has_many :assemblies_books
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates :published_at, presence: true
   belongs_to :author
+
+  has_many :assemblies_books
 end
