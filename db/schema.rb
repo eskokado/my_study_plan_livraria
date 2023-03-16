ba301fa5b8fc2a4281dbbceed9d7f8e83b610b31
eskokado <eskokado@gmail.com>
2023-03-16 12:42:20 -0300

    feat: add cnpj field in Supplier schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_213547) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_151058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_213547) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cnpj"
   end
 
   add_foreign_key "accounts", "suppliers"
