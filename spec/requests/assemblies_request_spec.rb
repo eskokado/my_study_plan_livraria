58c3419d106f6f0b7d13f94d3e0c7eed874f0ffd
eskokado <eskokado@gmail.com>
2023-03-16 07:41:48 -0300

    test: Assemblies POST /assemblies with valid params create a new Assembly with Parts and Books

--- spec/requests/assemblies_request_spec.rb
+++ spec/requests/assemblies_request_spec.rb
@@ -22,7 +22,17 @@ RSpec.describe "Assemblies", type: :request do
           post assemblies_path, params: { assembly: valid_attributes }
         }.to change(Assembly, :count).by(1)
       end
-    end
+
+      let(:valid_attributes) {
+        attributes_for(:assembly_with_parts_and_books)
+      }
+
+      it "creates a new Assembly with Parts and Books" do
+        expect {
+          post assemblies_path, params: { assembly: valid_attributes }
+        }.to change(Assembly, :count).by(1)
+      end
+   end
   end
 end
 
