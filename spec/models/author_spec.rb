4fd2faf4eeae771578da215684f146e0fd487e73
eskokado <eskokado@gmail.com>
2023-03-16 14:29:28 -0300

    test: add cpf field in Author Spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it 'is valid all attributes' do
-    author = create(:author)
+    author = build(:author, cpf: CPF.generate)
     expect(author).to be_valid
   end
 
   context 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:cpf) }
     it { should have_many(:books) }
   end
 end
