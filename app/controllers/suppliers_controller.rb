2db4b9f430633da43a2fe204ab8e082241ee8ee9
eskokado <eskokado@gmail.com>
2023-03-17 12:15:26 -0300

    feat: valid cnpj valid controller

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,3 +1,4 @@
+require 'cpf_cnpj'
 class SuppliersController < ApplicationController
   def create
     @supplier = Supplier.new(supplier_params)
@@ -17,6 +18,12 @@ class SuppliersController < ApplicationController
   end
 
   def account_params
-    params.require(:account).permit(:account_number)
+    params.require(:account).permit(:account_number, :verifier_digit)
+  end
+
+  def validate_cnpj
+    unless @supplier.valid_cnpj?
+      render json: { error: "Invalid CNPJ" }, status: :unprocessable_entity
+    end
   end
 end
