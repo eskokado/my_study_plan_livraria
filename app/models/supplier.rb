5e395df776d3406e97c586f73bcb849035d7c2fa
eskokado <eskokado@gmail.com>
2023-03-17 12:15:02 -0300

    feat: valid cnpj valid model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,8 +1,15 @@
 class Supplier < ApplicationRecord
   validates :name,  presence: true
   validates :cnpj, presence: true
+  validate :valid_cnpj?
 
   has_many :parts
 
   has_one :account
+
+  private
+  def valid_cnpj?
+    cnpj = CNPJ.new(self.cnpj)
+    cnpj.valid?
+  end
 end
