dbdec29e6a92de7ed2f7342cc1215445c006d9e3
eskokado <eskokado@gmail.com>
2023-03-17 18:08:04 -0300

    feat: validate verifier_digit

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,17 @@ class Account < ApplicationRecord
   validates :verifier_digit,  presence: true
 
   belongs_to :supplier
+
+  validate :validate_verifier_digit
+
+  def validate_verifier_digit
+    country_code = "BR"
+    bank_code = "0036"
+    branch_code = "0305"
+
+    iban = IBANTools::IBAN.new("#{country_code}#{verifier_digit}#{bank_code}#{branch_code}#{account_number}P1")
+    unless IBANTools::IBAN.valid?(iban.code)
+      errors.add(:verifier_digit, "is invalid")
+    end
+  end
 end
