5490d581e07fe1ea76833bf9d206ad94fc380194
eskokado <eskokado@gmail.com>
2023-03-17 10:33:30 -0300

    feat: add verifier_digit field in account schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_094640) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_103834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_094640) do
     t.bigint "supplier_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "verifier_digit"
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
   end
 
