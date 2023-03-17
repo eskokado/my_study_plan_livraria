9d696700dcc231b2b719ac20eb1ac6c574f10469
eskokado <eskokado@gmail.com>
2023-03-17 16:01:51 -0300

    fix: add validate isbn in Author model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,12 @@ class Book < ApplicationRecord
   belongs_to :author
 
   has_many :assemblies_books
+
+  validate :validate_isbn
+
+  def validate_isbn
+    unless ISBN.valid?(self.isbn)
+      errors.add(:isbn, "is invalid")
+    end
+  end
 end
