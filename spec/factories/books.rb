034693a8efae91842d45881e143d893ae8d48d9e
eskokado <eskokado@gmail.com>
2023-03-17 07:12:04 -0300

    feat: add field isbn in book factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :book do
     published_at { Faker::Date.between(from: '2023-01-01', to: '2023-03-15') }
+    isbn { Faker::Code.isbn(base: 13) }
     author
   end
 end
