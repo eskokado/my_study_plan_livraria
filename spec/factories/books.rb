1ebec5bbdfcd56499010972913e388cb2d1f7449
eskokado <eskokado@gmail.com>
2023-03-18 07:43:45 -0300

    feat: add title field in Books factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :book do
     published_at { Faker::Date.between(from: '2023-01-01', to: '2023-03-15') }
     isbn { Faker::Code.isbn(base: 13) }
+    title { Faker::Book.title }
     author
   end
 end
