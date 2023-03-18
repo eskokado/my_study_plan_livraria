93018d33442913bd975db70229b83eb8e6a01cbf
eskokado <eskokado@gmail.com>
2023-03-18 08:26:00 -0300

    feat: add name field in Parts schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_094307) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_18_111207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_094307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "supplier_id", null: false
+    t.string "name"
     t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
