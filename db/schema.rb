e5c63ea576fda949a83b7e5c9a4bfbfe1e0fb599
eskokado <eskokado@gmail.com>
2023-03-22 12:42:33 -0300

    feat: add column book_id in parts schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_111207) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_120436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_18_111207) do
     t.datetime "updated_at", null: false
     t.bigint "supplier_id", null: false
     t.string "name"
+    t.integer "book_id"
     t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
