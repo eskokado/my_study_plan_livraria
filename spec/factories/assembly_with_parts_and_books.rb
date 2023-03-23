2c4f022e51d01a97aca9ffab379b519db437fda3
eskokado <eskokado@gmail.com>
2023-03-23 10:12:41 -0300

    feat: create associate parts and books with assemblies N:M

--- spec/factories/assembly_with_parts_and_books.rb
+++ spec/factories/assembly_with_parts_and_books.rb
@@ -5,11 +5,26 @@ FactoryBot.define do
     transient do
       parts_count { 2 }
       books_count { 1 }
+      associated_books { [] } # Adicione esta linha
     end
 
     after(:create) do |assembly, evaluator|
-      create_list(:part, evaluator.parts_count, assemblies: [assembly])
-      create_list(:book, evaluator.books_count, assemblies: [assembly])
+      # Crie as peças e associe-as à montagem
+      evaluator.parts_count.times do
+        part = create(:part)
+        create(:assemblies_part, assembly: assembly, part: part)
+      end
+
+      # Crie os livros e associe-os à montagem
+      evaluator.books_count.times do
+        book = create(:book)
+        create(:assemblies_book, assembly: assembly, book: book)
+      end
+
+      # Associe os livros fornecidos através do atributo associated_books à montagem
+      evaluator.associated_books.each do |book|
+        create(:assemblies_book, assembly: assembly, book: book)
+      end
     end
   end
 end
