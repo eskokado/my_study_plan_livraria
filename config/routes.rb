c0f91cfe31c9ac4eadb9c371303420ad753e2d60
eskokado <eskokado@gmail.com>
2023-03-23 06:55:24 -0300

    feat: add routers of action get_author_with_books in AuthorsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get "home" => "home#index"
+  get 'authors/:id/with_books', to: 'authors#get_author_with_books', as: 'author_with_books'
+
   resources :authors
   resources :books
   resources :suppliers
   resources :parts
   resources :assemblies
+
 end
