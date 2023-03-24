4b29c6dcf47bd2f0271a55ccdcace4f66e7e08ec
eskokado <eskokado@gmail.com>
2023-03-24 18:13:13 -0300

    config: add config rspec_api_documentation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'rspec/json_expectations'
 require 'rspec-json_matchers'
+require 'rspec_api_documentation'
+require 'rspec_api_documentation/dsl'
 
+require File.expand_path('../../config/environment', __FILE__)
 RSpec.configure do |config|
   config.include RSpec::JsonMatchers::Matchers
   config.expect_with :rspec do |expectations|
@@ -12,4 +15,9 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+RspecApiDocumentation.configure do |config|
+  config.docs_dir = Rails.root.join('doc', 'api')
+  config.format = [:html]
 end
