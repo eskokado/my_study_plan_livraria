32ae6e2527afef758dc3f9d2de93c16a6e513463
eskokado <eskokado@gmail.com>
2023-03-24 18:13:48 -0300

    config: router for rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   get "home" => "home#index"
   get 'authors/:id/with_books', to: 'authors#get_author_with_books', as: 'author_with_books'
   get 'books/:id/assembly_parts_and_costs', to: 'books#get_book_with_assembly_parts_and_costs', as: 'book_with_assembly_parts_and_costs'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,17 +2,26 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+# require 'rails_helper'
+require 'swagger_helper'
+require 'rspec_api_documentation'
+require 'rspec_api_documentation/dsl'
+
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+
+# Rswag::Specs::SwaggerFormatter.configure do |config|
+#   config.swagger_root = Rails.root.to_s + '/swagger'
+#   config.include_failure_examples = true
+#   config.swagger_formatter_url = nil
+# end
+
 RSpec.configure do |config|
+  config.after(:each, type: :request) do |example|
+    example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+  end
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
@@ -25,4 +34,13 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.clean
   end
+
+  # config.include RSpecApiDocumentation::DSL
+end
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
 end
